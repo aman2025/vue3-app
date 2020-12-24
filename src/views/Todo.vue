244ee2d97cd0069924709dd3cfc0a04a9fc8cd33
aman2025 <2247367564@qq.com>
2020-12-24 17:29:57 +0800

    yeh

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -6,7 +6,7 @@
   </div>
   <!-- 引用ant-design-vue -->
   <a-layout>
-    <a-layout-header>待办事项12ctrl+shift+c</a-layout-header>
+    <a-layout-header>待办事项12ctrl+shift+c1</a-layout-header>
     <a-layout-content>
       <a-input-search v-model:value="todo" placeholder="新增待办事项" size="large" @search="addTodo">
         <template v-slot:enterButton>
